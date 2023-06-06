71851c17e8404fcb8fa8d9013aa217c403ee0da3
Shnai Johnson <shnai.johnson@gmail.com>
2023-06-06 14:09:00 +0800

    assets and components

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,3 +2,43 @@ class PagesController < ApplicationController
   def home
   end
 end
+
+def index
+  @items = Item.all
+
+  # Search results
+  if params[:query].present?
+    @items = Item.search_by_five_columns(params[:query])
+  else
+    @items = Item.all
+  end
+
+  # Additional logic for filtering
+  if params[:category].present?
+    @items = @items.where(category: params[:category])
+  end
+
+  # Additional logic for sorting
+  if params[:sort] == 'price_asc'
+    @items = @items.order(price: :asc)
+  elsif params[:sort] == 'price_desc'
+    @items = @items.order(price: :desc)
+  end
+
+  # Categorize items for the view
+  @dresses = @items.where(category: 'dresses')
+  @tops = @items.where(category: 'tops')
+  @bottoms = @items.where(category: 'bottoms')
+
+  # Determine the filtered category
+  @filtered_category = params[:category]
+
+  # Filtered category headers
+  @dresses_header = 'Dresses'
+  @tops_header = 'Tops'
+  @bottoms_header = 'Bottoms'
+end
+
+def show
+  @item = Item.find(params[:id])
+end
