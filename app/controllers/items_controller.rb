67df7eb719610569aee3f3fd3fef6eb1af1df58f
Bettina Tilrem <bettinatilrem@hotmail.com>
2023-06-06 16:57:44 +0800

    new and create almost done

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,24 @@
 class ItemsController < ApplicationController
   def index
   end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    @item.user_id = current_user.id
+    if @item.save
+      redirect_to items_path(@item), notice: "Item was successfully created, you can find it in your closet."
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:title, :brand, :category, :color, :original_price, :photo)
+  end
 end
