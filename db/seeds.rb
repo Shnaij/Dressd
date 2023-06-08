f7529b063466d463fd09b05bccced93fc3a47689
Bettina Tilrem <bettinatilrem@hotmail.com>
2023-06-08 11:22:55 +0800

    seeds done with 6 iterations on each category

--- db/seeds.rb
+++ db/seeds.rb
@@ -40,16 +40,18 @@ bettina = User.new(
 )
 bettina.save!
 
-dress_titles = ['Favorite dress', 'Party dress', 'Date night dress', 'Flattering dress', 'Summer dress', 'Casual dress']
-tops_titles = ['Best basic top', 'Favorite top', 'Summer top', 'Sexy top', 'I look good top', 'Casual top']
-bottoms_titles = ['Favorite pants', 'Comfy pants', 'Summer pants', 'Cool pants', 'My sexy pants', 'Casual pants']
-shoes_titles = ['Comfy shoes', 'Party shoes', 'Favorite shoes', 'Casual shoes', 'Summer shoes', 'Cool walk shoes']
+titles = {
+  Dresses: ['Favorite dress', 'Party dress', 'Date night dress', 'Flattering dress', 'Summer dress', 'Casual dress'],
+  Tops: ['Best basic top', 'Favorite top', 'Summer top', 'Sexy top', 'I look good top', 'Casual top'],
+  Bottoms: ['Favorite pants', 'Comfy pants', 'Summer pants', 'Cool pants', 'My fun pants', 'Casual pants'],
+  Shoes: ['Comfy shoes', 'Party shoes', 'Favorite shoes', 'Casual shoes', 'Summer shoes', 'Cool walk shoes']
+}
 
-types = Item::CATEGORIES
+categories = Item::CATEGORIES
 brands = ['Zara', 'Mango', 'Gucci', 'H&M', 'American Vintage', '& Other Stories', 'Urban Outfitters', 'TiMo', 'Brandz', 'Sister']
 colors = ['bright', 'flowery', 'multicolor']
 
-styles_array = ['sporty', 'casual', 'comfy', 'party', 'evening out']
+styles_array = ['Sporty', 'Casual', 'Comfy', 'Party', 'Evening out']
 styles_array.each do |style|
   Style.create(title: style)
 end
@@ -58,78 +60,83 @@ styles = Style.all
 
 user = [bettina, adinda, shnai, elizabeth]
 
-6.times do |num|
-  item = Item.new(
-    title: dress_titles[num],
-    category: "Dresses",
-    brand: brands.sample,
-    color: colors.sample,
-    original_price: Faker::Commerce.price(range: 100.0..500.0),
-    user: bettina
-  )
+# for each categories
+categories.each do |category|
+  puts "Creating #{category}"
+  6.times do |num|
+    item = Item.new(
+      title: titles[category.to_sym][num], # |category|
+      category: category, # |category|
+      brand: brands.sample,
+      color: colors.sample,
+      original_price: Faker::Commerce.price(range: 100.0..500.0),
+      user: bettina
+    )
 
-  # file = File.open(Rails.root.join("app/assets/images/#{category}#{num + 1}.jpeg"))
-  file = File.open(Rails.root.join("app/assets/images/dresses/hm_dress.jpeg"))
-  item.photo.attach(io: file, filename: 'image.jpeg', content_type: 'image/jpeg')
+    # add category folder path /bottoms
+    file = File.open(Rails.root.join("app/assets/images/#{category.downcase}/#{category}#{num + 1}.jpeg"))
+    # file = File.open(Rails.root.join("app/assets/images/dresses/hm_dress.jpeg"))
+    item.photo.attach(io: file, filename: 'image.jpeg', content_type: 'image/jpeg')
 
-  item.save!
+    item.save!
 
-  ItemStyle.create(item: item, style: styles.sample)
+    ItemStyle.create(item: item, style: styles.sample)
+  end
 end
 
-6.times do |num|
-  item = Item.new(
-    title: tops_titles[num],
-    category: "Tops",
-    brand: brands.sample,
-    color: colors.sample,
-    original_price: Faker::Commerce.price(range: 100.0..500.0),
-    user: bettina
-  )
+# 6.times do |num|
+#   item = Item.new(
+#     title: tops_titles[num],
+#     category: "Tops",
+#     brand: brands.sample,
+#     color: colors.sample,
+#     original_price: Faker::Commerce.price(range: 100.0..500.0),
+#     user: bettina
+#   )
 
-  # file = File.open(Rails.root.join("app/assets/images/#{category}#{num + 1}.jpeg"))
-  file = File.open(Rails.root.join("app/assets/images/tops  /hm_top.jpeg"))
-  item.photo.attach(io: file, filename: 'image.jpeg', content_type: 'image/jpeg')
+#   # file = File.open(Rails.root.join("app/assets/images/#{category}#{num + 1}.jpeg"))
+#   file = File.open(Rails.root.join("app/assets/images/tops  /hm_top.jpeg"))
+#   item.photo.attach(io: file, filename: 'image.jpeg', content_type: 'image/jpeg')
 
-  item.save
+#   item.save
 
-  ItemStyle.create(item: item, style: styles.sample)
-end
+#   ItemStyle.create(item: item, style: styles.sample)
+# end
 
-6.times do |num|
-  item = Item.new(
-    title: bottoms_titles[num],
-    category: "Bottoms",
-    brand: brands.sample,
-    color: colors.sample,
-    original_price: Faker::Commerce.price(range: 100.0..500.0),
-    user: bettina
-  )
+# 6.times do |num|
+#   item = Item.new(
+#     title: bottoms_titles[num],
+#     category: "Bottoms",
+#     brand: brands.sample,
+#     color: colors.sample,
+#     original_price: Faker::Commerce.price(range: 100.0..500.0),
+#     user: bettina
+#   )
 
-  # file = File.open(Rails.root.join("app/assets/images/#{category}#{num + 1}.jpeg"))
-  file = File.open(Rails.root.join("app/assets/images/bottoms/hm_pants.jpeg"))
-  item.photo.attach(io: file, filename: 'image.jpeg', content_type: 'image/jpeg')
+#   # file = File.open(Rails.root.join("app/assets/images/#{category}#{num + 1}.jpeg"))
+#   file = File.open(Rails.root.join("app/assets/images/bottoms/hm_pants.jpeg"))
+#   item.photo.attach(io: file, filename: 'image.jpeg', content_type: 'image/jpeg')
 
-  item.save
+#   item.save
 
-  ItemStyle.create(item: item, style: styles.sample)
-end
+#   ItemStyle.create(item: item, style: styles.sample)
+# end
 
-6.times do |num|
-  item = Item.new(
-    title: shoes_titles[num],
-    category: "Shoes",
-    brand: brands.sample,
-    color: colors.sample,
-    original_price: Faker::Commerce.price(range: 100.0..500.0),
-    user: bettina
-  )
+# 6.times do |num|
+#   item = Item.new(
+#     title: shoes_titles[num],
+#     category: "Shoes",
+#     brand: brands.sample,
+#     color: colors.sample,
+#     original_price: Faker::Commerce.price(range: 100.0..500.0),
+#     user: bettina
+#   )
 
-  # file = File.open(Rails.root.join("app/assets/images/#{category}#{num + 1}.jpeg"))
-  file = File.open(Rails.root.join("app/assets/images/shoes/hm_shoes.jpeg"))
-  item.photo.attach(io: file, filename: 'image.jpeg', content_type: 'image/jpeg')
+#   file = File.open(Rails.root.join("app/assets/images/#{category}#{num + 1}.jpeg"))
+#   # file = File.open(Rails.root.join("app/assets/images/shoes/hm_shoes.jpeg"))
+#   item.photo.attach(io: file, filename: 'image.jpeg', content_type: 'image/jpeg')
 
-  item.save
+#   item.save
 
-  ItemStyle.create(item: item, style: styles.sample)
-end
+#   ItemStyle.create(item: item, style: styles.sample)
+# end
