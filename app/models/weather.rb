97c4ff1b6c9e7834f1474e43d1841ac28f8c9d2f
elizabeth rivera <erivera424@gmail.com>
2023-06-12 16:43:28 +0800

    weather start

--- app/models/weather.rb
+++ app/models/weather.rb
@@ -1,5 +1,5 @@
 class Weather < ApplicationRecord
-  validates :temperature, presence: true
-  validates :description, presence: true
-  validates :city, presence: true
+  # validates :temperature, presence: true
+  # validates :description, presence: true
+  # validates :city, presence: true
 end
