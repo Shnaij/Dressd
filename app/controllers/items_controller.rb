fcc986df1edeac4024ab0e3e5786eacbaa1d86e1
Bettina Tilrem <bettinatilrem@hotmail.com>
2023-06-13 12:53:43 +0800

    notice on creation of new item

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -39,7 +39,7 @@ class ItemsController < ApplicationController
     @item = Item.new(item_params)
     @item.user_id = current_user.id
     if @item.save
-      redirect_to items_path(@item), notice: "Item was successfully created, you can find it in your closet."
+      redirect_to items_path, notice: "Item was successfully created and placed in your closet."
     else
       render :new, status: :unprocessable_entity
     end
